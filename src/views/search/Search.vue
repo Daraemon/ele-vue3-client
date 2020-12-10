a9ec5f80ad82ce15e9049b1a022dce69db78ef89
化无 <386372159@qq.com>
2020-12-10 11:53:49 +0800

    搜索页

--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -1,15 +1,37 @@
 <template>
-搜索
+<div class="search">
+  <van-nav-bar title="搜索" left-text="返回" left-arrow>
+  </van-nav-bar>
+  <van-search
+    v-model="value"
+    show-action
+    placeholder="请输入搜索关键词"
+    @search="onSearch"
+  >
+    <template #action>
+      <div @click="onSearch">搜索</div>
+    </template>
+  </van-search>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { NavBar, Search } from 'vant';
 
 export default defineComponent({
   components: {
+    [NavBar.name]: NavBar,
+    [Search.name]: Search,
   },
   setup() {
+    const searchText = ref<string>('');
+    function onSearch() {
+      console.log('点击了搜索');
+    }
     return {
+      searchText,
+      onSearch
     }
   },
 });
